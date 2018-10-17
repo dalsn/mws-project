7b6ccc1c9f503a9dc0284d3db9809b7922f635cf
Dalhatu <kulgee001@yahoo.com>
2018-10-17 23:19:03 +0100

    fix: Ready code for github pages deployment

--- sw.js
+++ sw.js
@@ -1,48 +1,34 @@
-let cacheName = 'restaurantReview-v2';
+'use strict';
 
-let filesToCache = [
-	'./',
-	'./index.html',
-	'./restaurant.html',
-	'./js/main.js',
-	'./js/restaurant_info.js',
-	'./js/db.js',
-	'./css/styles.css',
-	'https://unpkg.com/leaflet@1.3.1/dist/leaflet.css',
-	'https://unpkg.com/leaflet@1.3.1/dist/leaflet.js'
-];
+var cacheName = 'restaurantReview-v3';
 
-self.addEventListener('install', (e) => {
+var filesToCache = ['./', './index.html', './restaurant.html', './js/main.js', './js/restaurant_info.js', './js/db.js', './css/styles.css', 'https://unpkg.com/leaflet@1.3.1/dist/leaflet.css', 'https://unpkg.com/leaflet@1.3.1/dist/leaflet.js'];
+
+self.addEventListener('install', function (e) {
 	console.log('[ServiceWorker] Installed');
-	e.waitUntil(
-		caches.open(cacheName).then((cache) => {
-			return cache.addAll(filesToCache);
-		})
-	);
+	e.waitUntil(caches.open(cacheName).then(function (cache) {
+		return cache.addAll(filesToCache);
+	}));
 });
-
 
 /*
 * check for a response for request in cache
 * otherwise request resource over network and cache response
 */
-self.addEventListener('fetch', (e) => {
-	let requestUrl = new URL(e.request.url);
+self.addEventListener('fetch', function (e) {
+	var requestUrl = new URL(e.request.url);
 	if (requestUrl.protocol.startsWith('http')) {
-		e.respondWith(
-			caches.open(cacheName)
-			.then((cache) => {
-				return cache.match(e.request, { ignoreSearch: true }).then((response) => {
-					if (response) {
-						return response;
-					}
+		e.respondWith(caches.open(cacheName).then(function (cache) {
+			return cache.match(e.request, { ignoreSearch: true }).then(function (response) {
+				if (response) {
+					return response;
+				}
 
-					return fetch(e.request).then((networkResponse) => {
-						cache.put(e.request, networkResponse.clone());
-						return networkResponse;
-					})
-				})
-			})
-		);
+				return fetch(e.request).then(function (networkResponse) {
+					cache.put(e.request, networkResponse.clone());
+					return networkResponse;
+				});
+			});
+		}));
 	}
 });
